74e6c76c29ee8a7c98764c72c2765bc71ac0d62b
Marco Pastene <mpastene1996@outlook.com>
2025-05-26 05:10:41 +0000

    marcopastene13

---
+++ src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import Flask, jsonify
+from flask import request
+app = Flask(__name__)
+todos = [ { "label": "My first task", "done": False } ]
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+
+    todos.append(request_body)
+    
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if position < 0 or position >= len(todos):
+        return jsonify({"error": "Índice fuera de rango"}), 400
+    todos.pop(position)
+    return jsonify(todos)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
